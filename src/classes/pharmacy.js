115c11691ddc9eee0859458b1f86eb86e11b4ac5
Léandre <>
2025-03-03 00:12:01 +0100

    refactor: makes upfateBenefitValue() clearer using GPT

--- src/classes/pharmacy.js
+++ src/classes/pharmacy.js
@@ -10,57 +10,54 @@ export class Pharmacy {
   constructor(drugs = []) {
     this.drugs = drugs;
   }
+
   updateBenefitValue() {
-    for (var i = 0; i < this.drugs.length; i++) {
-      if (
-        this.drugs[i].name != "Herbal Tea" &&
-        this.drugs[i].name != "Fervex"
-      ) {
-        if (this.drugs[i].benefit > 0) {
-          if (this.drugs[i].name != "Magic Pill") {
-            this.drugs[i].benefit = this.drugs[i].benefit - 1;
-          }
-        }
-      } else {
-        if (this.drugs[i].benefit < 50) {
-          this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          if (this.drugs[i].name == "Fervex") {
-            if (this.drugs[i].expiresIn < 11) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-            if (this.drugs[i].expiresIn < 6) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.drugs[i].name != "Magic Pill") {
-        this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
-      }
-      if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
-          if (this.drugs[i].name != "Fervex") {
-            if (this.drugs[i].benefit > 0) {
-              if (this.drugs[i].name != "Magic Pill") {
-                this.drugs[i].benefit = this.drugs[i].benefit - 1;
-              }
-            }
-          } else {
-            this.drugs[i].benefit =
-              this.drugs[i].benefit - this.drugs[i].benefit;
-          }
-        } else {
-          if (this.drugs[i].benefit < 50) {
-            this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          }
-        }
-      }
-    }
+    this.drugs.forEach((drug) => {
+      this.updateBenefit(drug);
+      this.updateExpiration(drug);
+      this.handleExpiredDrug(drug);
+    });
 
     return this.drugs;
+  }
+
+  updateBenefit(drug) {
+    if (drug.name === "Magic Pill") return;
+
+    if (drug.name === "Herbal Tea") {
+      this.increaseBenefit(drug, 1);
+    } else if (drug.name === "Fervex") {
+      this.increaseBenefit(drug, 1);
+      if (drug.expiresIn < 11) this.increaseBenefit(drug, 1);
+      if (drug.expiresIn < 6) this.increaseBenefit(drug, 1);
+    } else {
+      this.decreaseBenefit(drug, 1);
+    }
+  }
+
+  updateExpiration(drug) {
+    if (drug.name !== "Magic Pill") {
+      drug.expiresIn -= 1;
+    }
+  }
+
+  handleExpiredDrug(drug) {
+    if (drug.expiresIn >= 0) return;
+
+    if (drug.name === "Herbal Tea") {
+      this.increaseBenefit(drug, 1);
+    } else if (drug.name === "Fervex") {
+      drug.benefit = 0;
+    } else if (drug.name !== "Magic Pill") {
+      this.decreaseBenefit(drug, 1);
+    }
+  }
+
+  increaseBenefit(drug, value) {
+    drug.benefit = Math.min(drug.benefit + value, 50);
+  }
+
+  decreaseBenefit(drug, value) {
+    drug.benefit = Math.max(drug.benefit - value, 0);
   }
 }
